1720875b3d827ddef3f7d6fb222297d71e613488
stevekevin1005 <qqwwee2006@gmail.com>
2017-02-02 13:39:21 +0800

    add html, ejs and jade example

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 var express = require('express');  
 var app = express(); 
 
-// app.set('view engine', 'jade');
-app.set('view engine', 'ejs');  
+app.set('view engine', 'jade');
+// app.set('view engine', 'ejs');  
 
 app.get('/', function(req, res) {  
   res.send('<h1>jade & express 範例</h1>');  
 });  
 
 app.get('/view/', function(req, res) {  
-  res.render('example');   
+  res.render('example', {  
+    title: 'Template example',  
+    fruits: ['Apple', 'Banana', 'Orange'],
+    person: {
+      name: 'steve',
+      sex: 'male'
+    },
+    html: "<h2>This is html code</h2>"
+  });   
 });  
 
 app.listen(3000, function() {  
