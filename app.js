02eb98286df591e26324a728aa7766f8cada4b69
stevekevin1005 <qqwwee2006@gmail.com>
2017-02-02 16:14:42 +0800

    add css

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express');  
 var app = express(); 
 
-app.set('view engine', 'jade');
-// app.set('view engine', 'ejs');  
+// app.set('view engine', 'jade');
+app.set('view engine', 'ejs');  
+
+app.use(express.static('assets'));
 
 app.get('/', function(req, res) {  
   res.send('<h1>jade & express 範例</h1>');  
