68dc16beccc66fccd7ce7ce8f6bc5a419d0525ac
niconame <hazelluf@gmail.com>
2017-02-06 22:21:16 +0800

    add files

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 var express = require('express');  
 var app = express(); 
 
-// app.set('view engine', 'jade');
 app.set('view engine', 'ejs');  
 
 app.use(express.static('assets'));
 
 app.get('/', function(req, res) {  
-  res.send('<h1>jade & express 範例</h1>');  
+  res.send('<h1>Week2 Homework: Express2</h1>');  
 });  
 
 app.get('/view/', function(req, res) {  
   res.render('example', {  
-    title: 'Template example',  
-    fruits: ['Apple', 'Banana', 'Orange'],
+    title: 'week2 Homework: Express2',  
+    cats: ['Garfield', 'Hello Kitty', 'Tom'],
     person: {
-      name: 'steve',
-      sex: 'male'
+      name: 'Nico',
+      sex: 'female'
     },
-    html: "<h2>This is html code</h2>"
+    html: "<h3>Here is code block test.</h3>"
   });   
 });  
 
+app.use('/', index);
 app.listen(3000, function() {  
   console.log('Listening on port 3000');  
 });   
