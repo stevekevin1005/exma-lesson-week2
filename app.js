87eaecb9e990624b2d7463457a4593c7ec1d3596
何筱莉 <hsiaoli@hexiaolideMacBook-Air.local>
2017-02-08 20:28:05 +0800

    Second upload

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
 var express = require('express');  
 var app = express(); 
+var http=require('http');
+const path = require("path");
+const port=3000;
 
-// app.set('view engine', 'jade');
-app.set('view engine', 'ejs');  
+app.set('view engine', 'jade');  
+app.set('views', path.join(__dirname, 'views'));
+console.log(__dirname);
 
-app.use(express.static('assets'));
+app.use(express.static("assets"));
 
 app.get('/', function(req, res) {  
-  res.send('<h1>jade & express 範例</h1>');  
+  res.render('example.jade', {  
+    title: 'Title test (JADE)',  
+    heros: ['Talon', 'Talon', 'Talon'],
+    me: {
+      name: 'Talon',
+      hobby: 'running in the summon’s rift'
+    },
+    html: "<h2><i>Talon~</i></h2>"
+  });    
 });  
 
-app.get('/view/', function(req, res) {  
-  res.render('example', {  
-    title: 'Template example',  
-    fruits: ['Apple', 'Banana', 'Orange'],
-    person: {
-      name: 'steve',
-      sex: 'male'
+app.get('/test', function(req, res) {  
+  res.send('<h1>範例</h1>');  
+}); 
+
+app.get('/ejs', function(req, res) {  
+  res.render('example.ejs', {  
+    title: 'Title test (EJS)',  
+    heros: ['Talon', 'Katarina', 'Other Assassins'],
+    me: {
+      name: 'JJJason',
+      sex: 'unknown'
     },
-    html: "<h2>This is html code</h2>"
+    html: "<h2><i>This is html code</i></h2>"
   });   
 });  
 
-app.listen(3000, function() {  
-  console.log('Listening on port 3000');  
-});   
+http.createServer(app)
+.listen(port, ()=> {
+  console.log('App listening on port ' + port + ' !');
+})
